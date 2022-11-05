88b98c72ec95dfb41fa1f088391520cc16bda062
Firi0n <88585181+Firi0n@users.noreply.github.com>
2022-11-05 19:41:51 +0100

    Revert "components"
    This reverts commit 3f4749ab28aa9c6723fefb08bc98e1d6e66dfc6c.

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,3 +1,10 @@
+﻿let inputField = document.querySelector('input');
+let btn = document.querySelector('#button');
+let errorOne = document.querySelector('#message');
+let errorContainer = document.querySelector('.message-container');
+let logoutButton = document.querySelector('#logout-button');
+let airCondition;
+
 
 btn.onclick = async function getData() {
 
@@ -90,3 +97,38 @@ btn.onclick = async function getData() {
 };
     }
 
+
+
+
+// search bar opens up or closes on click
+let openSearchBar = document.querySelector('.open_close_search');
+
+openSearchBar.addEventListener('click', function () {
+
+    
+    if (document.querySelector('.input-container').style.visibility == 'hidden') {
+        document.querySelector('.input-container').style.visibility = 'visible'
+    } else {
+       document.querySelector('.input-container').style.visibility = 'hidden'
+    }
+})
+
+
+//search fires when enter button is pressed on INPUT
+
+inputField.addEventListener('keypress', function(e){
+    if(e.key === 'Enter' && inputField.value ==""){
+        
+        errorOne.style.visibility = 'visible';
+        errorOne.textContent = 'Please type a city name';
+        document.cursor.style.display = 'none';
+
+    } else if(e.key === 'Enter' && inputField.value != ""){
+        btn.onclick();
+    }
+})
+
+//preventing the page to refresh when logout button is pressed or clicked..
+logoutButton.addEventListener('click', function(e){
+    e.preventDefault();
+})
